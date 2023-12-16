dce0540ea22a54f0efbfac1e28594d49ef6bbadc
Pinja Alanne <pinja.alanne@gmail.com>
2023-12-16 19:15:30 +0200

    delete extra css

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -2,15 +2,13 @@ function EventCard({ event }) {
 
     return (
         <div className="card">
-            <div className="card-wrap">
-                <button className="closebtn">x</button>
-                <h3 className="eventheader">{event.name.fi}</h3>
-                <img className="eventimg" src={event.images[0].url} alt='no img'></img>
-                <div className="eventtext-wrap">
-                    <p className="starttime">Event date: {(event.start_time).split('', 10)}</p>
-                    <p className="eventtext">{event.short_description.fi}</p>
-                </div >
-            </div>
+            <button className="closebtn">x</button>
+            <h3 className="eventheader">{event.name.fi}</h3>
+            <img className="eventimg" src={event.images[0].url} alt='no img'></img>
+            <div className="eventtext-wrap">
+                <p className="starttime">Event date: {(event.start_time).split('', 10)}</p>
+                <p className="eventtext">{event.short_description.fi}</p>
+            </div >
         </div>
     );
 }
